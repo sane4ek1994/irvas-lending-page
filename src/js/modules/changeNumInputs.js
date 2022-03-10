8235a62a2bebf623a6d29797398abc22acf6fd0b
Alexandr Naumov <63644905+sane4ek1994@users.noreply.github.com>
2022-03-10 21:19:56 +0300

    update-modal-forms

---
+++ src/js/modules/changeNumInputs.js
@@ -0,0 +1,11 @@
+const changeNumInputs = (selector) => {
+    const numInputs = document.querySelectorAll(selector);
+
+    numInputs.forEach(item => {
+        item.addEventListener('input', () => {
+            item.value = item.value.replace(/\D/, '');
+        });
+    });
+};
+
+export default changeNumInputs;
--- src/js/modules/forms.js
+++ src/js/modules/forms.js
@@ -1,13 +1,11 @@
-const forms = () => {
-    const form = document.querySelectorAll('form'),
-          inputs = document.querySelectorAll('input'),
-          phoneInputs = document.querySelectorAll('input[name="user_phone"]');
+import changeNumInputs from "./changeNumInputs";
 
-    phoneInputs.forEach(item => {
-        item.addEventListener('input', () => {
-            item.value = item.value.replace(/\D/, '');
-        });
-    });
+const forms = (state) => {
+    const form = document.querySelectorAll('form'),
+          inputs = document.querySelectorAll('input');
+
+
+    changeNumInputs('input[name="user_phone"]');
 
     const message = {
         loading: 'Загрузка...',
@@ -40,6 +38,11 @@ const forms = () => {
             item.appendChild(statusMessage);
 
             const formData = new FormData(item);
+            if (item.getAttribute('data-calc') === 'end') {
+                for (let key in state) {
+                    formData.append(key, state[key]);
+                }
+            }
 
             postData('assets/server.php', formData)
                 .then(res => {
